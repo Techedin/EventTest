e28a0baad06234e34966f8a6cff0e2208a156a45
Techedin <bendog0302@gmail.com>
2022-10-03 22:21:37 -0400

    Assignment 3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,17 +50,41 @@ namespace EventTest
             this.Card2 = new System.Windows.Forms.PictureBox();
             this.Card1 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.RainbowSentance = new System.Windows.Forms.TabPage();
+            this.RainbowTextbox = new System.Windows.Forms.RichTextBox();
+            this.Rainbowbutton = new System.Windows.Forms.Button();
+            this.RainbowText = new System.Windows.Forms.Label();
+            this.SoundTab = new System.Windows.Forms.TabPage();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CowSound = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CatSound = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.MusicButton = new System.Windows.Forms.Button();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.GroceryTextbox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.removeItem = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.Card3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Card2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Card1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.RainbowSentance.SuspendLayout();
+            this.SoundTab.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            resources.ApplyResources(this.button1, "button1");
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
@@ -143,9 +167,9 @@ namespace EventTest
             // 
             // button5
             // 
-            resources.ApplyResources(this.button5, "button5");
             this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
             this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            resources.ApplyResources(this.button5, "button5");
             this.button5.Name = "button5";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -184,17 +208,163 @@ namespace EventTest
             // 
             // pictureBox1
             // 
-            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.BackgroundImage = global::EventTest.Properties.Resources._5_The_Player_Guild;
             this.pictureBox1.Image = global::EventTest.Properties.Resources._5_The_Player_Guild;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.RainbowSentance);
+            this.tabControl1.Controls.Add(this.SoundTab);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // RainbowSentance
+            // 
+            this.RainbowSentance.BackColor = System.Drawing.Color.Black;
+            this.RainbowSentance.Controls.Add(this.RainbowTextbox);
+            this.RainbowSentance.Controls.Add(this.Rainbowbutton);
+            this.RainbowSentance.Controls.Add(this.RainbowText);
+            resources.ApplyResources(this.RainbowSentance, "RainbowSentance");
+            this.RainbowSentance.Name = "RainbowSentance";
+            // 
+            // RainbowTextbox
+            // 
+            this.RainbowTextbox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            resources.ApplyResources(this.RainbowTextbox, "RainbowTextbox");
+            this.RainbowTextbox.ForeColor = System.Drawing.SystemColors.Window;
+            this.RainbowTextbox.Name = "RainbowTextbox";
+            // 
+            // Rainbowbutton
+            // 
+            resources.ApplyResources(this.Rainbowbutton, "Rainbowbutton");
+            this.Rainbowbutton.Name = "Rainbowbutton";
+            this.Rainbowbutton.UseVisualStyleBackColor = true;
+            this.Rainbowbutton.Click += new System.EventHandler(this.Rainbowbutton_Click);
+            // 
+            // RainbowText
+            // 
+            resources.ApplyResources(this.RainbowText, "RainbowText");
+            this.RainbowText.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.RainbowText.Name = "RainbowText";
+            // 
+            // SoundTab
+            // 
+            this.SoundTab.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.SoundTab.Controls.Add(this.label8);
+            this.SoundTab.Controls.Add(this.CowSound);
+            this.SoundTab.Controls.Add(this.label7);
+            this.SoundTab.Controls.Add(this.CatSound);
+            this.SoundTab.Controls.Add(this.label6);
+            this.SoundTab.Controls.Add(this.MusicButton);
+            resources.ApplyResources(this.SoundTab, "SoundTab");
+            this.SoundTab.Name = "SoundTab";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // CowSound
+            // 
+            resources.ApplyResources(this.CowSound, "CowSound");
+            this.CowSound.Name = "CowSound";
+            this.CowSound.UseVisualStyleBackColor = true;
+            this.CowSound.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // CatSound
+            // 
+            resources.ApplyResources(this.CatSound, "CatSound");
+            this.CatSound.Name = "CatSound";
+            this.CatSound.UseVisualStyleBackColor = true;
+            this.CatSound.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // MusicButton
+            // 
+            resources.ApplyResources(this.MusicButton, "MusicButton");
+            this.MusicButton.Name = "MusicButton";
+            this.MusicButton.UseVisualStyleBackColor = true;
+            this.MusicButton.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.SpringGreen;
+            this.tabPage1.Controls.Add(this.removeItem);
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.GroceryTextbox);
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click_1);
+            // 
+            // GroceryTextbox
+            // 
+            resources.ApplyResources(this.GroceryTextbox, "GroceryTextbox");
+            this.GroceryTextbox.Name = "GroceryTextbox";
+            this.GroceryTextbox.TextChanged += new System.EventHandler(this.textBox5_TextChanged_1);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // removeItem
+            // 
+            resources.ApplyResources(this.removeItem, "removeItem");
+            this.removeItem.Name = "removeItem";
             // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Maroon;
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.Card3);
             this.Controls.Add(this.Card2);
             this.Controls.Add(this.Card1);
@@ -222,6 +392,13 @@ namespace EventTest
             ((System.ComponentModel.ISupportInitialize)(this.Card2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Card1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.RainbowSentance.ResumeLayout(false);
+            this.RainbowSentance.PerformLayout();
+            this.SoundTab.ResumeLayout(false);
+            this.SoundTab.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -249,6 +426,26 @@ namespace EventTest
         private System.Windows.Forms.PictureBox Card1;
         private System.Windows.Forms.PictureBox Card2;
         private System.Windows.Forms.PictureBox Card3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage RainbowSentance;
+        private System.Windows.Forms.TabPage SoundTab;
+        private System.Windows.Forms.Label RainbowText;
+        private System.Windows.Forms.Button Rainbowbutton;
+        private System.Windows.Forms.RichTextBox RainbowTextbox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button CatSound;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button MusicButton;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button CowSound;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox GroceryTextbox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox removeItem;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,21 @@
 Ben Williams 9-12-22 
 Estimated Hours Taken: 2 Hours Basic 
 GUI and Input Systems
+
+Ben Williams 9-21-22
+Estimated Hours Taken: 2 Hours 
+Files,Card Draw Loop
+
+Ben Williams 9-22-22
+Estimated Hours Taken: 2 Hours 
+Comments and more loops
+
+Ben Williams 10-2-22
+Estimated Hours Taken: 2-4 Hours
+Adding more string manipulation, lists, design
 */
+
+
 
 using System;
 using System.Collections.Generic;
@@ -25,16 +39,20 @@ namespace EventTest
     {
 
         string textFile = "file.txt";
+
         Image[] images = new Image[14];
         PictureBox[] cardDisplay = new PictureBox[3];
+
         int number;
         int die;
         double numToRound;
-        bool isDrawn;
+
+        List<string> groceries = new List<string>();
 
         public Form1()
         {
             InitializeComponent();
+            //Card Images
             images[0] = Image.FromFile("C:/Users/Techedin/source/repos/CSC202 Projects/EventTest/1.JPG");
             images[1] = Image.FromFile("C:/Users/Techedin/source/repos/CSC202 Projects/EventTest/2.PnG");
             images[2] = Image.FromFile("C:/Users/Techedin/source/repos/CSC202 Projects/EventTest/3.PnG");
@@ -49,9 +67,11 @@ namespace EventTest
             images[11] = Image.FromFile("C:/Users/Techedin/source/repos/CSC202 Projects/EventTest/12.PnG");
             images[12] = Image.FromFile("C:/Users/Techedin/source/repos/CSC202 Projects/EventTest/13.PnG");
             images[13] = Image.FromFile("C:/Users/Techedin/source/repos/CSC202 Projects/EventTest/14.PnG");
+            //References to my picture boxes
             cardDisplay[0] = Card1;
             cardDisplay[1] = Card2;
             cardDisplay[2] = Card3;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -61,13 +81,14 @@ namespace EventTest
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            //Set text in text box to a referencable string
             string str = textBox4.Text;
-
-
-
+            //check string for all lower case, uppercase, and numbers 0-9 for validation
             if (Regex.IsMatch(str, "^[a-zA-Z0-9]*$"))
             {
+
                 MessageBox.Show("valid string a-z, 0-9");
+                //Use streamwrite to write string into selected textFile
                 using (StreamWriter fileWriter = File.AppendText(textFile))
                 {
                     fileWriter.WriteLine(textBox4.Text.ToString());
@@ -81,14 +102,18 @@ namespace EventTest
         }
         private void button5_Click(object sender, EventArgs e)
         {
+            //Check if the file exists
             if (File.Exists(textFile))
             {
+                //Grab all lines in file and assign it to a string array
                 string[] linesInFile = File.ReadAllLines(textFile);
+                //use a stream reader to read the line in file
                 using (StreamReader file = new StreamReader(textFile))
+                    //forech loop (Runs loop for every string in the textFile)
                     foreach (string line in linesInFile)
                     {
                         MessageBox.Show(line);
-                        textBox4.Text = line;
+
                     }
             }
             else
@@ -117,8 +142,10 @@ namespace EventTest
 
         private void RoundButton_Click(object sender, EventArgs e)
         {
+            //Check Text Box for a double
             if (double.TryParse(textBox2.Text, out numToRound))
             {
+                //Round to two decimal points
                 numToRound = Math.Round((Double)numToRound, 2);
                 textBox2.Text = numToRound.ToString();
             }
@@ -130,6 +157,7 @@ namespace EventTest
 
         private void RollDiceButton(object sender, EventArgs e)
         {
+            //Random number between 1 and 20 and some math
             Random random = new Random();
             die = random.Next(1, 20);
             MessageBox.Show("Random Die = " + die);
@@ -142,6 +170,7 @@ namespace EventTest
 
         private void IntegerCheckButton(object sender, EventArgs e)
         {
+            //Check to see is there is a number in text box
             if (int.TryParse(textBox1.Text, out number))
             {
                 MessageBox.Show("Valid Number");
@@ -154,25 +183,179 @@ namespace EventTest
 
         private void button6_Click(object sender, EventArgs e)
         {
+            //Delete refernced file
             File.Delete(textFile);
         }
 
         private void DrawCards_Click(object sender, EventArgs e)
         {
+            //Make a random
             Random random = new Random();
+            //Call function
             DrawCards(3);
+            //Draw cards function
             void DrawCards(int i)
             {
+                //A For Loop
                 for (int k = 0; k < i; k++)
                 {
+                    //Change card displays to the drawn card image
                     cardDisplay[k].Image = images[random.Next(0, 13)];
                 }
-                isDrawn = true;
             }
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //Simple while loop
+            int i = 1;
+            while (i <= 5)
+            {
+                MessageBox.Show(i.ToString()); ;
+                i++;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            //simple do while loop
+            int i = 1;
+            do
+            {
+                MessageBox.Show(i.ToString()); ;
+                i++;
+            }
+            while (i < 5);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rainbowbutton_Click(object sender, EventArgs e)
+        {
+            Random rndColor = new Random();
+            // Save selection
+            var oldStart = RainbowTextbox.SelectionStart;
+            var oldLength = RainbowTextbox.SelectionLength;
+
+
+            // Select the text to change
+            for (int i = 0; i < RainbowTextbox.TextLength; i++)
+            {
+                //Got the color randomizer from the web but made it apply to each char in a string via counting up to the length of the string.
+                Color randomColor = Color.FromArgb(rndColor.Next(256), rndColor.Next(256), rndColor.Next(256));
+                RainbowTextbox.Select(i, 1);
+                // Change color
+                RainbowTextbox.SelectionColor = randomColor;
+            }
+
+            // Restore selection
+            RainbowTextbox.Select(oldStart, oldLength);
+        }
+
+        //All the sound were pulled from royalty free sites 
+        private void button8_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Techedin\source\repos\CSC202 Projects\EventTest\cat.wav");
+                player.Stop();
+                player.Play();
+            }
+            catch
+            {
+                MessageBox.Show("WAV file doesn't exist. Please add one to the file");
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //try and catch so code doesn't crash if media isn't found
+            try
+            {
+                //Initalize Player
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Techedin\source\repos\CSC202 Projects\EventTest\music.wav");
+                //Stop any previous sound
+                player.Stop();
+                //Play sound
+                player.Play();
+            }
+            catch
+            {
+                MessageBox.Show("WAV file doesn't exist. Please add one to the file");
+            }
+
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\Techedin\source\repos\CSC202 Projects\EventTest\cow.wav");
+                player.Stop();
+                player.Play();
+            }
+            catch
+            {
+                MessageBox.Show("WAV file doesn't exist. Please add one to the file");
+            }
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click_1(object sender, EventArgs e)
+        {
+            //Add to list
+            groceries.Add(GroceryTextbox.Text);
+            //Read list
+            for (int i = 0; i < groceries.Count; i++)
+            {
+                MessageBox.Show(groceries[i]);
+            }
+
+
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i < groceries.Count; i++)
+            {
+                //Checks the text box to remove 
+                if (groceries[i].Equals(removeItem.Text))
+                {
+                    groceries.RemoveAt(i);
+                }
+                try
+                {
+                    MessageBox.Show(groceries[i]);
+                }
+                catch
+                {
+
+                }
+
+
+            }
+
 
         }
     }
